8ba6bee19151ff1b239ba5e9893568b457145870
Siva-Komma <kommasivajyothi22@gmail.com>
2023-03-17 19:52:16 +0530

    Employee PayRoll ER Diagram

---
+++ EmployeePayRoll/EmployeeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRoll
+{
+    public class EmployeeModel
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public string Gender{ get; set; }
+
+
+    }
+}
---
+++ EmployeePayRoll/EmployeeRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRoll
+{
+    public class EmployeeRepo
+    {
+        public static string connectionstring = "";
+        sql
+        public bool AddEmployee(EmployeeModel employeeModel)
+        {
+            try
+            {
+                using(this.GetHashCode())
+                {
+                    SqlCommand command = new SqlCommand("SpAddEmployeeDetails", this.connection);
+                    command.CommandType = System.Data.CommandType.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmployeeName", employeeModel.EmployeeName);
+                    command.Parameters.AddWithValue("")
+                }
+            }
+
+        }
+    }
+}
